fcc7a9b954b0e5a651e7790dce9ee94393db83a9
BandersGit <andy.i.flaskan@gmail.com>
2022-11-07 11:55:23 +0100

    Created functions to take number values as well as the desired operator

--- src/App.java
+++ src/App.java
@@ -1,5 +1,54 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Input the first number:");
+        int firstNumber = AskInput(input);
+
+        System.out.println("Input one of these operators (+, -, *, /)");
+        String operator = AskOperator(input);
+
+        System.out.println("Input the first number:");
+        int secondNumber = AskInput(input);
+
+        System.out.println(firstNumber + operator + secondNumber);
+        
+        input.close();
+    }
+
+    public static int AskInput(Scanner input){
+        Scanner numInput = input;
+        int number;
+        
+        do {
+            try {
+                String in = numInput.nextLine();
+                number = Integer.parseInt(in);
+                break;
+            } catch (Exception e) {
+                System.out.println("Not a valid input, please try again");
+            }
+        } while (true);
+
+        return number;
+    }
+
+    public static String AskOperator(Scanner input){
+        Scanner opInput = input;
+        String operator;
+
+        while (true) {
+            operator = opInput.nextLine();
+
+            if (operator.equals("+") || operator.equals("-") || operator.equals("*") || operator.equals("/")) {
+                break;
+            }else{
+                System.out.println("Not a valid input, please try again");
+            }
+        }
+
+        return operator;
     }
 }
